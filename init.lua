4adbfa06adf32e5fab589b6dfdd716a5adf66267
Martin <martin.bilger@relyit.se>
2024-10-22 21:51:00 +0200

    Fix keymaps

--- init.lua
+++ init.lua
@@ -227,8 +227,10 @@ vim.opt.rtp:prepend(lazypath)
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
+
 require 'custom.remap'
 require 'custom.set'
+--require 'custom.harpoon'
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
